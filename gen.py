18547c8936b7f822cc36e026af4896cc130f1c83
Keenan Graham <ksgraham@stanford.edu>
2024-07-02 18:37:56 -0700

    Update

--- gen.py
+++ gen.py
@@ -257,14 +257,13 @@ def clean_schema(schema):
                     for v in value:
                         resolved_subtypes.update(subtypes[v])
                     value = list(sorted(resolved_subtypes))
-                    res = [{'type': 'string'}]
-                    res.append([
+                    res = [
                         {
-                            '$ref': f"#/components/schemas/{v}",
-                            'title': f"{v}"
+                            '$ref': f"#/components/schemas/{v}"
                         }
                         for v in value
-                    ])
+                    ]
+                    res.append({'type': 'string'})
                     cleaned = {}
                     cleaned['oneOf'] = res
                     return cleaned
